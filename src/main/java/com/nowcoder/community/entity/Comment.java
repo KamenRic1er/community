717892e0c52adfa2e082ce5d96fd4059b2219bc4
EndlessLearning <pcwl1017@163.com>
2023-01-16 15:01:30 +0800

    添加评论功能实现

--- src/main/java/com/nowcoder/community/entity/Comment.java
+++ src/main/java/com/nowcoder/community/entity/Comment.java
@@ -6,7 +6,9 @@ public class Comment {
 
     private int id;
     private int userId;
+    // 被评论类型（帖子/回复）
     private int entityType;
+    // 被评论id
     private int entityId;
     private int targetId;
     private String content;
