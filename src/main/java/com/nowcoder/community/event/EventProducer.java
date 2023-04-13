0a8730b415f49c838ba09c969f5885471bc9a1fa
EndlessLearning <pcwl1017@163.com>
2023-04-13 11:43:19 +0800

    Quartz注释

--- src/main/java/com/nowcoder/community/event/EventProducer.java
+++ src/main/java/com/nowcoder/community/event/EventProducer.java
@@ -1,10 +1,15 @@
 package com.nowcoder.community.event;
 
 import com.alibaba.fastjson2.JSONObject;
+import com.nowcoder.community.controller.LoginController;
 import com.nowcoder.community.entity.Event;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
 
 /**
  * @Author: 少不入川
@@ -13,13 +18,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class EventProducer {
 
+    private static final Logger logger = LoggerFactory.getLogger(EventProducer.class);
+
     @Autowired
     private KafkaTemplate kafkaTemplate;
 
     // 处理事件
     public void fireEvent(Event event){
         // 将事件发送到指定的主题
-        kafkaTemplate.send(event.getTopic(), JSONObject.toJSONString(event));
+        //kafkaTemplate.send(event.getTopic(), JSONObject.toJSONString(event));
+
+        // 添加回调函数，为消息发送失败记录日志
+        ListenableFuture<SendResult<String, Object>> future = kafkaTemplate.send(event.getTopic(), JSONObject.toJSONString(event));
+        future.addCallback(result -> logger.info("生产者成功发送消息到topic:{} partition:{}的消息", result.getRecordMetadata().topic(), result.getRecordMetadata().partition()),
+                ex -> logger.error("生产者发送消失败，原因：{}", ex.getMessage()));
+
     }
 
 
