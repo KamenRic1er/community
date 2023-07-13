6489ce63eb8df52972125b481e0ed580677bd131
EndlessLearning <pcwl1017@163.com>
2023-07-13 21:27:47 +0800

    添加线程池

--- src/main/java/com/nowcoder/community/config/ThreadPoolConfig.java
+++ src/main/java/com/nowcoder/community/config/ThreadPoolConfig.java
@@ -1,13 +1,33 @@
 package com.nowcoder.community.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.ThreadPoolExecutor;
 
 @Slf4j
 @Configuration
 @EnableScheduling
 @EnableAsync
 public class ThreadPoolConfig {
+
+    @Bean
+    public ThreadPoolTaskExecutor executor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+
+        taskExecutor.setCorePoolSize(10);
+        taskExecutor.setMaxPoolSize(100);
+        taskExecutor.setQueueCapacity(50);
+        taskExecutor.setKeepAliveSeconds(200);
+        taskExecutor.setThreadNamePrefix("async-");
+        taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        taskExecutor.initialize();
+
+        return taskExecutor;
+    }
+
 }
