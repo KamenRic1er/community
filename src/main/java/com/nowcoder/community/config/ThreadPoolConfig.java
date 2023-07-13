060a41cffda016d39185be6349d5d61d0cad0774
EndlessLearning <pcwl1017@163.com>
2023-07-13 21:21:56 +0800

    使用lombok，提高代码简洁度

--- src/main/java/com/nowcoder/community/config/ThreadPoolConfig.java
+++ src/main/java/com/nowcoder/community/config/ThreadPoolConfig.java
@@ -1,12 +1,13 @@
 package com.nowcoder.community.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Slf4j
 @Configuration
 @EnableScheduling
-// 使注解Async生效，使用该注解的方法可以在多线程的环境下被异步的调用
 @EnableAsync
 public class ThreadPoolConfig {
 }
--- src/main/java/com/nowcoder/community/entity/LoginTicket.java
+++ src/main/java/com/nowcoder/community/entity/LoginTicket.java
@@ -1,7 +1,10 @@
 package com.nowcoder.community.entity;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class LoginTicket {
 
     private int id;
@@ -10,54 +13,4 @@ public class LoginTicket {
     private int status;
     private Date expired;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public String getTicket() {
-        return ticket;
-    }
-
-    public void setTicket(String ticket) {
-        this.ticket = ticket;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public Date getExpired() {
-        return expired;
-    }
-
-    public void setExpired(Date expired) {
-        this.expired = expired;
-    }
-
-    @Override
-    public String toString() {
-        return "LoginTicket{" +
-                "id=" + id +
-                ", userId=" + userId +
-                ", ticket='" + ticket + '\'' +
-                ", status=" + status +
-                ", expired=" + expired +
-                '}';
-    }
 }
