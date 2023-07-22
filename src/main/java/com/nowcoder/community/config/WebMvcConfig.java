231d9810ba59cca8056d30fd6ea6fba9c1a67e10
EndlessLearning <pcwl1017@163.com>
2023-07-22 16:17:02 +0800

    将统计DAU、UV的功能改用AOP实现，同时取消该Interceptor的注册

--- src/main/java/com/nowcoder/community/config/WebMvcConfig.java
+++ src/main/java/com/nowcoder/community/config/WebMvcConfig.java
@@ -22,8 +22,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private MessageInterceptor messageInterceptor;
 
-    @Autowired
-    private DataInterceptor dataInterceptor;
+//    @Autowired
+//    private DataInterceptor dataInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -36,8 +36,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(messageInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
         // 收集UV DAU的拦截器
-        registry.addInterceptor(dataInterceptor)
-                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+//        registry.addInterceptor(dataInterceptor)
+//                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
     }
 
 }
--- src/main/java/com/nowcoder/community/controller/interceptor/DataInterceptor.java
+++ src/main/java/com/nowcoder/community/controller/interceptor/DataInterceptor.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@Component
+//@Component
 public class DataInterceptor implements HandlerInterceptor {
 
     @Autowired
