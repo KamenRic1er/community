721b53c26b35251633c01385e2f8a971f11bdfb3
EndlessLearning <pcwl1017@163.com>
2023-07-23 14:51:49 +0800

    添加默认CacheManager，为用户缓存、帖子缓存分别添加CacheManager

--- src/main/java/com/nowcoder/community/config/CaffeineConfig.java
+++ src/main/java/com/nowcoder/community/config/CaffeineConfig.java
@@ -1,12 +1,14 @@
 package com.nowcoder.community.config;
 
-import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import com.nowcoder.community.util.CommonUtil;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -16,12 +18,36 @@ import java.util.concurrent.TimeUnit;
 @Configuration
 public class CaffeineConfig {
 
+
+    // 默认CacheManager: 如果未指定cacheManager则使用该配置
+    @Primary
     @Bean
     public CacheManager cacheManager(){
-        CaffeineCacheManager cacheManager=new CaffeineCacheManager();
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
         cacheManager.setCaffeine(Caffeine.newBuilder()
                 .initialCapacity(128)
-                .maximumSize(1024)
+                .maximumSize(256)
+                .expireAfterWrite(60, TimeUnit.SECONDS));
+        return cacheManager;
+    }
+
+
+    @Bean
+    public CacheManager userCacheManager(){
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("user");
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                .initialCapacity(200)
+                .maximumSize(500)
+                .expireAfterWrite(60, TimeUnit.SECONDS));
+        return cacheManager;
+    }
+
+    @Bean
+    public CacheManager postCacheManager(){
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("post");
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                .initialCapacity(100)
+                .maximumSize(250)
                 .expireAfterWrite(180, TimeUnit.SECONDS));
         return cacheManager;
     }
