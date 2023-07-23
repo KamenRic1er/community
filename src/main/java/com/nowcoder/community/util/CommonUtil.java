facde7556783a61f891d4ef7e11f54249cabed38
EndlessLearning <pcwl1017@163.com>
2023-07-23 14:49:53 +0800

    添加打乱过期时间方法，避免大量缓存数据同时过期导致缓存雪崩

---
+++ src/main/java/com/nowcoder/community/util/CommonUtil.java
@@ -0,0 +1,17 @@
+package com.nowcoder.community.util;
+
+import java.util.Random;
+
+/**
+ * @Author: 少不入川
+ * @Date: 2023/7/22 21:31
+ */
+public class CommonUtil {
+
+    private static Random random = new Random();
+
+    public static long getRandomExpireTime(long expireTime){
+        return expireTime + (long) random.nextInt((int)expireTime / 10);
+    }
+
+}
