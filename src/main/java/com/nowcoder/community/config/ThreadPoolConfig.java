f02bdde61f72604b254dc7f41af2feb8a79c9991
EndlessLearning <pcwl1017@163.com>
2023-10-25 11:13:06 +0800

    自定义线程池

--- src/main/java/com/nowcoder/community/config/ThreadPoolConfig.java
+++ src/main/java/com/nowcoder/community/config/ThreadPoolConfig.java
@@ -15,13 +15,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 @EnableAsync
 public class ThreadPoolConfig {
 
-    // 如果serviceA、serviceB对象之间相互依赖，serviceA和serviceB总一个一个会先实例化，而serviceA或serviceB里面使用了@Async注解，会导致循环依赖异常：
-    // org.springframework.beans.factory.BeanCurrentlyInCreationException
-    // 解决办法：
-    // 1.在A类上加@Lazy，保证A对象实例化晚于B对象
-    // 2.不使用@Async注解，通过自定义异步工具类发起异步线程（线程池）
-    // 3.不要让@Async的Bean参与循环依赖
-
+    // 自定义线程池，避免@Async注解导致的OOM
     @Bean
     public ThreadPoolTaskExecutor executor() {
         ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
